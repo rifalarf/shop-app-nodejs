dd2b3a5e3d88e7fbcdc069bea45ecfe20744b93d
rifalarf <rifalarifrahman23@gmail.com>
2024-08-29 19:09:58 +0700

    final

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require ('express')
 const app = express()
 const mongoose = require ('mongoose');
 const Product = require ('./models/product');
-const { log, Console } = require('console');
+const methodOveride = require ('method-override')
 
 // konek mongose
 mongoose.connect('mongodb://localhost/shop_db').then((result) => {
@@ -16,6 +16,7 @@ mongoose.connect('mongodb://localhost/shop_db').then((result) => {
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended : true}))
+app.use(methodOveride('_method'))
 
 app.get('/', (req, res) => {
     res.send('konek kak')
@@ -27,9 +28,15 @@ app.get('/', async (req, res) => {
 })
 
 app.get('/products', async (req, res) => {
-    const products = await Product.find({})
-    res.render('products/index', {products})
-    
+    const {category}  = req.query
+    console.log(category);
+    if (category) {
+        const products = await Product.find({category})
+        res.render('products/index', {products, category})
+    } else{
+        const products = await Product.find({})
+        res.render('products/index', {products, category : 'All'})
+    }
 })
 
 app.get('/products/create', (req, res) => {
@@ -56,6 +63,17 @@ app.get('/products/:id/edit', async (req, res) => {
   res.render('products/edit', { product })
 })
 
+app.put('/products/:id', async (req,res) => {
+    const {id} = req.params
+    const product = await Product.findByIdAndUpdate(id, req.body, {runValidators:true})
+    res.redirect(`/products/${product._id}`) 
+})
+
+app.delete('/products/:id', async (req,res) => {
+    const {id} = req.params
+    await Product.findByIdAndDelete(id)
+    res.redirect('/products')
+})
 
 app.listen(3000, () => {
     console.log('konek http://localhost:3000');
